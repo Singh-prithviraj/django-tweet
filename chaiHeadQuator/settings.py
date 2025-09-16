c9392e8648469f07fe470d67fddff7915915997f
prithviraj singh <prithvirajchouhan2003@gmail.com>
2025-09-16 08:58:31 +0530

    test

--- chaiHeadQuator/settings.py
+++ chaiHeadQuator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -81,6 +81,9 @@ DATABASES = {
     }
 }
 
+DATABASES["default"] = dj_database_url.parse("postgresql://prithvi_django_tweet_user:1d0KUUoaXF5QkfjdKjPuqz0SDa8sJMwL@dpg-d3452c6r433s7391te8g-a.singapore-postgres.render.com/prithvi_django_tweet")
+
+# postgresql://prithvi_django_tweet_user:1d0KUUoaXF5QkfjdKjPuqz0SDa8sJMwL@dpg-d3452c6r433s7391te8g-a.singapore-postgres.render.com/prithvi_django_tweet
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- chaiHeadQuator/urls.py
+++ chaiHeadQuator/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,6 @@ urlpatterns = [
     
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns  += staticfiles_urlpatterns()
